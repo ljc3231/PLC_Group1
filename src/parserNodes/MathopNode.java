4cec1de117a1f87d99ec2882890d2befed3fefea
Alex Zou <alexzou26@gmail.com>
2024-10-15 19:03:06 -0400

    coded mathopNode

--- src/parserNodes/MathopNode.java
+++ src/parserNodes/MathopNode.java
@@ -1,4 +1,41 @@
 package parserNodes;
+import exceptionFiles.*;
+import java.util.ArrayList;
+import provided.*;
 
-public class MathopNode {
+public class MathopNode implements JottTree{
+    private final String mathOp;
+
+    MathopNode(String s) {
+        this.mathOp = s;
+    }
+
+    public static MathopNode parse(ArrayList<Token> tokens) throws EndOfFileException, JottException {
+        if (tokens.isEmpty()) {
+            throw new EndOfFileException("mathop");
+        }
+        
+        Token t = tokens.get(0);
+
+        if (!t.getTokenType().equals(TokenType.REL_OP)) {
+            throw new JottException("MathopNode", "Expected mathop, instead recieved \"" + t.getToken() + "\".", t.getLineNum());
+        }
+
+        return new MathopNode(t.getToken());
+    }
+
+    @Override
+    public String convertToJott() {
+        return this.mathOp;
+    }
+
+    @Override
+    public boolean validateTree() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }

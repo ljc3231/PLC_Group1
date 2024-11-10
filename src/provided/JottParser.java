0a15629b888830df2325f94eaefd344c3a3557bb
Liam Cummings <ljc3231@rit.edu>
2024-11-10 17:37:50 -0500

    upadated JottParser

--- src/provided/JottParser.java
+++ src/provided/JottParser.java
@@ -23,11 +23,15 @@ public class JottParser {
     public static JottTree parse(ArrayList<Token> tokens){
         try {
             return ProgramNode.parse(tokens);
-        } catch (EndOfFileException e) {
-            return null;
-        } catch (JottException e) {
+        } catch (Exception e) {
             System.err.println(e.getMessage());
             return null;
         }
+ //       catch (EndOfFileException e) {
+//            return null;
+//        } catch (JottException e) {
+//            System.err.println(e.getMessage());
+//            return null;
+        }
     }
-}
+
